0172d8e94fda7c80d12b84c8078b91b21a329d1f
Joni Kahari <joni.kahari@gmail.com>
2025-03-18 10:21:09 +0200

    Muistiinpano omaan komponenttiin.

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+function Note({ notes }) {
+  return (
+    <div className="border-1 bg-neutral-50">
+      <p>Muistiinpanon id: {notes.id}</p>
+      <p className="font-thin">
+        {notes.timestamp} {notes.course.name} (id {notes.course.id})
+      </p>
+      <p className="font-extralight"> {notes.text}</p>
+    </div>
+  );
+}
+export default Note;
