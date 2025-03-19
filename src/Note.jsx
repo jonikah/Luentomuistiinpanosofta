5c57e37b0c7d0fd526b7d0504522aa59b7f5fe39
Joni Kahari <joni.kahari@gmail.com>
2025-03-19 16:06:32 +0200

    Testitulosteet kommentoitu.

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,10 +1,10 @@
 function Note({ notes }) {
   return (
-    <div className="border-1 bg-neutral-50">
-      <p>Muistiinpanon id: {notes.id}</p>
+    <div className="border-1 bg-neutral-50 p-5 mb-3 ">
+      {/* <p>Muistiinpanon id: {notes.id}</p>
       <p className="font-thin">
         {notes.timestamp} {notes.course.name} (id {notes.course.id})
-      </p>
+      </p> */}
       <p className="font-extralight"> {notes.text}</p>
     </div>
   );
